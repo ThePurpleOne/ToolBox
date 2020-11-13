8c6c4bec89b616519196721f633e4b157a897984
Jonas STIRNEMANN <ThePurpleOne@users.noreply.github.com>
2020-11-13 15:19:26 +0100

    [ADD] Added bouncing ball on walls

--- P5_js/sketch.js
+++ P5_js/sketch.js
@@ -1,3 +1,10 @@
+let gVelX = 30;
+let gVelY = 3;
+let gXPos = 500;
+let gYPos = 500;
+let gRadius = 50;
+
+
 function preload()
 {
 
@@ -9,14 +16,20 @@ function setup()
 	createCanvas(windowWidth , windowHeight );
 	print("Window Width : " + windowWidth);
 	print("Window Height : " + windowHeight);
+	frameRate(120);
 	background(0);
 }
 
 function draw() 
 {
-	background(color('hsla(160, 100%, 50%, 0.5)'));
-	textSize(300);
-	textStyle()
-	text('ELECZOO', windowWidth / 2 - 300 * 2.5, windowHeight / 2 + 100 );
+	background(0);
+
+	gXPos += gVelX;
+	gYPos += gVelY;
+
+	if(gXPos > (windowWidth - gRadius / 2) || gXPos < gRadius / 2 ){gVelX *= -1;}
+	if(gYPos > (windowHeight - gRadius / 2) || gYPos < gRadius / 2 ){gVelY*= -1;}
+
+	circle(gXPos, gYPos, gRadius);
 }
 
