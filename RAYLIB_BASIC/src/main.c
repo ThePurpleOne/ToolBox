b3f1551ea5a0b3f0adb439b5f4ba403b4c48c2c8
ThePurpleOne <jonas@stirnemann.xyz>
2021-09-24 17:28:08 +0200

    Added Raylib basic

---
+++ RAYLIB_BASIC/src/main.c
@@ -0,0 +1,37 @@
+/*
+	* CODE MAIN
+	* Author : Jonas S.
+	* Date   : 24/09/2021
+	! Basic Project Raylib
+	? Source : www.raylib.com
+*/
+
+#include <stdio.h>
+#include "raylib.h"
+
+int main()
+{
+	const int screenWidth = 1000;
+    const int screenHeight = 600;
+	
+    InitWindow(screenWidth, screenHeight, "BASIC WINDOW");
+	
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose())
+    {
+        BeginDrawing(); // ! DRAWING
+
+		DrawFPS(10, 10); 
+		ClearBackground(LIGHTGRAY);
+
+		DrawRectangleRec((Rectangle) {100, 100, 300, 300}, LIME);
+		DrawCircle(250, 250, 50, SKYBLUE);
+
+        EndDrawing(); // ! END DRAWING
+    }
+
+    CloseWindow();
+
+	return 0;
+}
