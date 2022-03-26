5012c62d74289a2c7e8d1c57977df240a4bc4f50
Jonas Stirnemann <jonas.stirnemann@etu.hesge.ch>
2022-03-26 11:46:21 +0100

    Refactored shaders into 2 files and reading them correctly

--- OPEN_GL_BASIC/src/test.c
+++ OPEN_GL_BASIC/src/test.c
@@ -9,88 +9,78 @@
 #include <stdlib.h>
 #include <string.h>
 
-static void parse_shader(char* file_path)
+#define RESET	"\033[0m"
+#define RED		"\033[31m" /* Red */
+#define GREEN	"\033[32m" /* Green */
+#define WHITE	"\033[37m" /* White */
+#define YELLOW	"\033[33m" /* Yellow */
+#define BLUE	"\033[34m" /* Blue */
+#define MAGENTA "\033[35m" /* Magenta */
+#define CYAN	"\033[36m" /* Cyan */
+
+/**
+ * @brief Read the shaders from files and return an array of strings
+ * 		  containing the shaders
+ *
+ * @param fpath_vertex
+ * @param fpath_fragment
+ * @return char** array[0] = vertex shader,
+ * 				  array[1] = fragment shader
+ */
+char** read_shaders(char* fpath_vertex, char* fpath_fragment)
 {
-	FILE* fp = fopen(file_path, "r");
-	if (fp == NULL)
+	char line[100] = { 0 };
+
+	// ! READING VERTEX SHADER
+	char  buff_vertex[1000] = { 0 };
+	FILE* fp_vertex			= fopen(fpath_vertex, "r");
+	if (fp_vertex == NULL)
 	{
-		printf("Failed to open file %s\n", file_path);
-		return;
+		fprintf(stderr, "Failed to open file %s\n", fpath_vertex);
+		return NULL;
 	}
-	printf("DEBUG - AFTER OPENNING FILE \n");
-
-	// ! GET TO THE BEGINNING OF SHADER VERTEX
-	while (1) // ? LOOK FOR SHADER VERTEX
+	while (!feof(fp_vertex))
 	{
-		char* line = malloc(sizeof(char) * 100);
-		getline(&line, NULL, fp);
-		printf("DEBUG - GETLINE PROBLEM\n");
-		printf("LINE :  %s\n", line);
+		fgets(line, 100, fp_vertex);
+		strcat(buff_vertex, line);
+	}
 
-		if (strstr(line, "#shader vertex") != NULL)
-		{
-			printf("DEBUG - STR STR  PROBLEM\n");
-			break;
-		}
-		printf("DID NOT BREAK\n");
-	} // QUIT WHEN FOUND "shader vertex"
+	// ! READING FRAGMENT SHADER
+	char  buff_fragment[1000] = { 0 };
+	FILE* fp_fragment		  = fopen(fpath_fragment, "r");
+	if (fp_fragment == NULL)
+	{
+		fprintf(stderr, "Failed to open file %s\n", fpath_fragment);
+		return NULL;
+	}
+	while (!feof(fp_fragment))
+	{
+		fgets(line, 100, fp_fragment);
+		strcat(buff_fragment, line);
+	}
 
-	// printf("DEBUG - FOUND SHADER VERTEX\n");
+	fclose(fp_vertex);
+	fclose(fp_fragment);
 
-	// char buff_shader_vertex[1000];
-	// while (1) // ? READ THE SHADER UNTIL END SHADER VERTEX
-	//{
-	//	if (!feof(fp))
-	//		return;
+	char** output_array	   = malloc(sizeof(char*) * 2);
+	char*  output_vertex   = strdup(buff_vertex);
+	char*  output_fragment = strdup(buff_fragment);
 
-	//	char* line = NULL;
-	//	getline(&line, NULL, fp);
+	output_array[0] = output_vertex;
+	output_array[1] = output_fragment;
 
-	//	if (strstr(line, "#end shader vertex") != NULL)
-	//		break;
-
-	//	// Copy the shader string line by line into the buffer
-	//	strcat(line, buff_shader_vertex);
-	//} // QUIT WHEN FOUND "end shader vertex"
-
-	//// ! GET TO THE BEGINNING OF FRAGMENT SHADER
-	// while (1) // ? LOOK FOR FRAGMENT SHADER
-	//{
-	//	if (!feof(fp))
-	//		return;
-
-	//	char* line = NULL;
-	//	getline(&line, NULL, fp);
-
-	//	if (strstr(line, "#fragment vertex") != NULL)
-	//		break;
-	//} // QUIT WHEN FOUND "shader vertex"
-
-	// char buff_shader_fragment[1000];
-	// while (1) // ? READ THE SHADER UNTIL END SHADER FRAGMENT
-	//{
-	//	if (!feof(fp))
-	//		return;
-
-	//	char* line = NULL;
-	//	getline(&line, NULL, fp);
-
-	//	if (strstr(line, "#end shader vertex") != NULL)
-	//		break;
-
-	//	// Copy the shader string line by line into the buffer
-	//	strcat(line, buff_shader_fragment);
-	//} // QUIT WHEN FOUND "end shader fragment"
-
-	// fclose(fp);
-
-	// printf("%s\n", buff_shader_vertex);
-	// printf("%s\n", buff_shader_fragment);
+	return output_array;
 }
-
 
 int main()
 {
-	parse_shader("src/shaders/base.shader");
-	printf("TEST \n");
+	char** shaders = read_shaders("src/shaders/base_vertex.shader",
+								  "src/shaders/base_fragment.shader");
+
+	printf("%s\n\n", shaders[0]);
+	printf("%s\n\n", shaders[1]);
+
+	free(shaders[0]);
+	free(shaders[1]);
+	free(shaders);
 }
